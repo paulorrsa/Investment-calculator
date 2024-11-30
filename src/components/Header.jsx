139085fb52564826c7db93222555ce0df5e4ba5e
paulorrsa <paulo26301@outlook.com>
2024-11-30 15:52:48 -0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import logo from '../assets/investment-calculator-logo.png'
+export function Header() {
+  return (
+    <header id='header'>
+      <img src={ logo} alt="" />
+      <h1>Investment Calculator</h1>
+    </header>
+  )
+}
